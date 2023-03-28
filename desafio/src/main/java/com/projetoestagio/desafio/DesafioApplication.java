ebc8b6ed491a27603df91e8dab887a032fec3823
André Rodrigues <mrnexusrider@hotmail.com>
2023-03-28 16:43:56 -0300

    Correção bean

--- desafio/src/main/java/com/projetoestagio/desafio/DesafioApplication.java
+++ desafio/src/main/java/com/projetoestagio/desafio/DesafioApplication.java
@@ -2,8 +2,10 @@ package com.projetoestagio.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication()
+@ComponentScan(basePackages = {"com.projetoestagio.desafio.repositories"})
 public class DesafioApplication {
 
 	public static void main(String[] args) {
